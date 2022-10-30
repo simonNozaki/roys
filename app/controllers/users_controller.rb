65671876c45a8fd5d3adb7238fe5af92a0647859
snozaki <snozaki@netprotections.co.jp>
2022-10-30 22:11:39 +0900

    ADD: user signup and redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(get_validated_user_params)
+    if @user.save
+      # redirect_to user_url(@user) と等価
+      flash[:success] = "Welcome to the Roys!"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+    def get_validated_user_params
+      params
+        .require(:user)
+        .permit(
+          :name,
+          :email,
+          :password,
+          :password_confirmation
+        )
+    end
 end
