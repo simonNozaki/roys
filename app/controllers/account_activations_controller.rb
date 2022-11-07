29a5a6dd588e814a9fbe14e2b2c264eb3e8d36e7
snozaki <snozaki@netprotections.co.jp>
2022-11-07 22:13:45 +0900

    ADD: 有効化トークンの認証アクション

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,15 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by_email(params[:email])
+    # 認証済みかつ有効ではない
+    if user && !user.activated? && user.is_activated?(params[:id])
+      user.activate
+      log_in(user)
+      flash[:success] = "Account activated!"
+      redirect_to(user)
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to(root_url)
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,20 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     # nil-safe operator https://mitrev.net/ruby/2015/11/13/the-operator-in-ruby/
-    if @user&.authenticate(params[:session][:password])
+    unless @user&.authenticate(params[:session][:password])
+      flash.now[:danger] = 'Invalid email/password combination'
+      return render 'new'
+    end
+
+    if @user.activated?
       log_in(@user)
       params[:session][:remember_me] == REMEMBER_ME_CHECK_ON ? remember(@user) : forget(@user)
       redirect_back_or(@user)
     else
-      # 次のリクエストが来るとnilになる
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      flash[:warning] = <<-EOS
+Account not activated. Check your email for the activation link.
+      EOS
+      redirect_to(root_url)
     end
   end
 
