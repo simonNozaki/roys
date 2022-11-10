bbf2567679ddee9ab9c5ff718f77d9190024255f
snozaki <snozaki@netprotections.co.jp>
2022-11-10 15:38:05 +0900

    MOD: 壊れているテストの修正

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -14,7 +14,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect to 'destroy' on not logged in" do
     assert_no_difference 'Micropost.count' do
-      delete(microposts_path(@micropost))
+      delete(micropost_path(@micropost))
     end
     assert_redirected_to(login_url)
   end
